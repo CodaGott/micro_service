121ee784274ddeeeca0e57349627b2cfcb71e1b9
codagott <greatchidozie@gmail.com>
2025-09-05 21:52:08 +0100

    added payment service

---
+++ PaymentServicer/src/main/java/com/dozie/PaymentServicer/PaymentServicerApplication.java
@@ -0,0 +1,13 @@
+package com.dozie.PaymentServicer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServicerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServicerApplication.class, args);
+	}
+
+}
---
+++ PaymentServicer/src/test/java/com/dozie/PaymentServicer/PaymentServicerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dozie.PaymentServicer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServicerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
